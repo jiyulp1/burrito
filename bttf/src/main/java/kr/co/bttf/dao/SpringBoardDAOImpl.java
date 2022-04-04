4bab311dd8fa2367ebd7d279bf2aadf4517c6ec5
Sujin <sjnnus@gmail.com>
2022-04-04 23:30:47 +0900

    board_html

--- bttf/src/main/java/kr/co/bttf/dao/SpringBoardDAOImpl.java
+++ bttf/src/main/java/kr/co/bttf/dao/SpringBoardDAOImpl.java
@@ -49,7 +49,7 @@ public class SpringBoardDAOImpl implements SpringBoardDAO {
 	
 	@Override
 	public void springcategory2(int post_id) throws Exception {
-		sql.update(namespace + ".category2", post_id);
+		sql.update(namespace + ".springcategory2", post_id);
 		
 	}
 	
